3a0a682a80d8735538aa0466fed7e615973d35cf
ogabriel <gabrieloliver8991@gmail.com>
2020-04-04 20:27:00 -0300

    add http server to index

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
-const Logger = require('./logger')
-const logger = new Logger();
+const http = require('http');
+const path = require('path');
+const fs = require('fs');
 
-logger.on('message', (data) => console.log('Called listener', data));
+const server = http.createServer((req, res) => {
+	if(req.url === '/') {
+		res.end('<h1>Homeman</h1>');
+	}
+});
 
-logger.log('test')
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => console.log(`Server running on ${PORT}`));
