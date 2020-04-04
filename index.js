3c20965cb7046603db3a1f70ddc7c6fdc31d913d
ogabriel <gabrieloliver8991@gmail.com>
2020-04-04 18:22:11 -0300

    use class to show person

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
-const person = require('./person')
+const Person = require('./person');
+const person1 = new Person('John Doe', 30);
 
-console.log(person);
+person1.greeting();
--- person.js
+++ person.js
@@ -1,6 +1,12 @@
-const person = {
-	name: 'John doe',
-	age: 30
+class Person {
+	constructor(name, age) {
+		this.name = name;
+		this.age = age;
+	}
+
+	greeting() {
+		console.log(`My name is ${this.name} and I'm ${this.age}`)
+	}
 }
 
-module.exports = person;
+module.exports = Person;
