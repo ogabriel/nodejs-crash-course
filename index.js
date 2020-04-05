6db7296d55f1b23b724c80faed654f703415daf3
ogabriel <gabrieloliver8991@gmail.com>
2020-04-04 21:27:06 -0300

    add endpoints to assets and apis

--- index.js
+++ index.js
@@ -2,9 +2,43 @@ const http = require('http');
 const path = require('path');
 const fs = require('fs');
 
+const Person = require('./person');
+
 const server = http.createServer((req, res) => {
 	if(req.url === '/') {
-		res.end('<h1>Homeman</h1>');
+		fs.readFile(path.join(__dirname, 'public', 'index.html'), (err, contents) => {
+			if(err) throw err;
+
+			res.writeHead(200, { 'Content-Type': 'text/html' });
+			res.end(contents);
+		})
+	}
+
+	if(req.url === '/about') {
+		fs.readFile(path.join(__dirname, 'public', 'about.html'), (err, contents) => {
+			if(err) throw err;
+
+			res.writeHead(200, { 'Content-Type': 'text/html' });
+			res.end(contents);
+		})
+	}
+
+	if(req.url === '/api/user/1') {
+		const user = new Person('Bob Smith', 34)
+
+		res.writeHead(200, { 'Content-Type': 'text/html' });
+		res.end(JSON.stringify(user));
+	}
+
+
+	if(req.url === '/api/users') {
+		const user1 = new Person('Bob Smith', 34)
+		const user2 = new Person('Johm Doe', 30)
+
+		const users = [user1, user2]
+
+		res.writeHead(200, { 'Content-Type': 'text/html' });
+		res.end(JSON.stringify(users));
 	}
 });
 
